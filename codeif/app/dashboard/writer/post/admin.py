a79e9ab88d5a6ba0f26f4412c51cdfd30163d05a
Nema Chandra Goswami <83756388+Nym-Git@users.noreply.github.com>
2021-08-26 17:32:10 +0530

    Update admin.py

--- codeif/app/dashboard/writer/post/admin.py
+++ codeif/app/dashboard/writer/post/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import post
+from .models import post, Category
 # Register your models here.
 
 admin.site.register(post)
+admin.site.register(Category)
