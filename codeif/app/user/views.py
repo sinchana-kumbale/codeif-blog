4ea2d04fdc82f84253b3720b44640ac585fe2d61
HARIHARAN B P <bphariharan1301@gmail.com>
2021-08-27 09:48:06 +0530

    Verification on process Dont edit the user/views.py file for time being

--- codeif/app/user/views.py
+++ codeif/app/user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import CustomUser
 from django.contrib.auth import login,logout 
 from django.contrib.auth.forms import UserCreationForm
@@ -14,5 +14,17 @@ from django.urls import reverse_lazy, reverse
 class UserRegisterView(generic.CreateView):
   form_class = SignupFORMS
   template_name  = 'registration/register.html'
-  success_url = reverse_lazy('login')
+  success_url = reverse_lazy('verify')
+
+# class UserVerifyView():
+#   form_class = SignupFORMS
+#   template_name = 'registration/verify.html'
+#   success_url = reverse_lazy('login')
+
+
+def UserVerifyView(request):
+  if request.method == 'POST':
+    pass
+
+  return render(request, 'registration/verify.html')
 
